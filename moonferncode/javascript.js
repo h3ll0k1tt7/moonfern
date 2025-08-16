a6afcbec2c3716750e9051bdc18ece19bd67268a
hellokitty <133889145+h3ll0k1tt7@users.noreply.github.com>
2025-08-16 03:09:28 -0700

    Add files via upload

---
+++ moonferncode/javascript.js
@@ -0,0 +1,9 @@
+function privacypolicy() {
+    alert("We care about your privacy! We don't use cookies, trackers, or collect your data unless you choose to share it. If you fill out our contact form, we'll only use your name and email to reply to you. We use Formspree to process messages, and we never sell or share your info.");
+}
+function termsandconditions() {
+    alert("When you shop with Moonfern, you agree that all items are described as clearly as possible most are pre-loved, so tiny imperfections may exist and that's part of their charm. Prices are in USD and can change if needed, we only deliver within lebanon and delivery fees and times confirmed upon order, also Moonfern isn't responsible for delays caused by the delivery service or incorrect addresses provided by the customer")
+}
+function returnpolicy() {
+    alert("We hope you adore Moonfern goodies, but if an item arrives damaged or very diffrent from its description, you must contact us within 3 days to request a return, items must be in the same condition you received them, once we receive and check the return we'll issue a refund (excluding delivery costs), for hygiene reasons we cannot accept returns of jewelry, herbs, or certain personal items, and to start a return, email us with your order info and photos if needed.");
+}
